b346aaf2ee5df3b323dd1f5410f347f02bdbeeb0
Matthew Richardson <matthew.r.richardson@vanderbilt.edu>
2016-04-23 18:04:52 -0500

    Add map parsing

--- maintrap.py
+++ maintrap.py
@@ -6,42 +6,200 @@ import math
 
 if __name__ == '__main__':
 	
-	# Define a TrapZone and some obstacles to test with
-	# Be sure to enter them in counterclockwise order for the triangulation
-	zone = [	
-		(100,400),
-		(150,250),
-		(50,100),
-		(400,100),
-		(400,250),
-		(250,400),
-		(250,250)
-	]
+	option = input("(1) Use existing map\n(2) Manually input map\n(3) Use default map (Which is map #3)\nChoose option: ")
 	
-	xs = [math.floor(x[0]) for x in zone]
-	ys = [math.floor(x[1]) for x in zone]
+	zone = []
+	obstacles = []
+	if option==1:
+		map = input("Which map? (1, 2, or 3): ")
+		if map==1:
+			zone = [
+				(0,0),
+				(200,0),
+				(200,125),
+				(0,125)
+			]
+			obstacles = [
+				[
+					(35,45),
+					(55,45),
+					(55,60),
+					(35,60)
+				], 
+				[
+					(30,95),
+					(40,95),
+					(40,105),
+					(30,105)
+				],
+				[
+					(100,85),
+					(140,85),
+					(140,115),
+					(100,115)
+				],
+				[
+					(150,30),
+					(160,30),
+					(160,40),
+					(150,40)
+				]
+				
+			]
+		if map==2:
+			zone = [
+				(0,0),
+				(200,0),
+				(200,125),
+				(0,125)
+			]
+			obstacles = [
+				[
+					(10,65),
+					(20,65),
+					(20,75),
+					(10,75)
+				], 
+				[
+					(30,30),
+					(50,30),
+					(50,50),
+					(30,50)
+				],
+				[
+					(80,55),
+					(105,55),
+					(105,80),
+					(80,80)
+				],
+				[
+					(145,90),
+					(160,90),
+					(160,115),
+					(145,115)
+				],
+				[
+					(140,30),
+					(160,30),
+					(160,50),
+					(140,50)
+				]
+			]	
+		if map==3:
+			zone = [
+				(0,0),
+				(150,0),
+				(150,125),
+				(0,125)
+			]
+			obstacles = [
+				[
+					(15,25),
+					(35,25),
+					(35,45),
+					(15,45)
+				], 
+				[
+					(40,90),
+					(50,90),
+					(50,100),
+					(40,100)
+				],
+				[
+					(80,50),
+					(90,50),
+					(90,60),
+					(80,60)
+				],
+				[
+					(105,75),
+					(125,75),
+					(125,95),
+					(105,95)
+				],
+				[
+					(105,15),
+					(145,15),
+					(145,35),
+					(105,35)
+				]
+			]
 	
-	obstacles = [
-		[
-			(275,125),
-			(350,125),
-			(350,180),
-			(275,180)
+	if option==2:
+		print("INPUT FLY-ZONE")
+		num = input("How many points?: ")
+		for i in range(1,num+1):
+			tuple = input("Point "+str(i)+" (x,y): ")
+			zone = zone + [tuple]
+		
+		new = input("New obstacle? (y/n): ")
+		while new=="y":
+			obstacle = []
+			num = input("How many points?: ")
+			for i in range(1,num+1):
+				tuple = input("Point "+str(i)+" (x,y): ")
+				obstacle = obstacle + [tuple]
+			obstacles = obstacles + obstacle
+			new = input("New obstacle? (y/n): ")
+			
+			
+	if option==3:
+		zone = [
+			[
+				(0,0),
+				(150,0),
+				(150,125),
+				(0,125)
+			]
 		]
-	]
+		obstacles = [
+			[
+				(15,25),
+				(35,25),
+				(35,45),
+				(15,45)
+			], 
+			[
+				(40,90),
+				(50,90),
+				(50,100),
+				(40,100)
+			],
+			[
+				(80,50),
+				(90,50),
+				(90,60),
+				(80,60)
+			],
+			[
+				(105,75),
+				(125,75),
+				(125,95),
+				(105,95)
+			],
+			[
+				(105,15),
+				(145,15),
+				(145,35),
+				(105,35)
+			]
+		]
 	
 	zone = shapely.geometry.Polygon(zone)
 	obstacles = [shapely.geometry.Polygon(o) for o in obstacles]
+	obstacles1 = []
 	
 	# Obtain input from the user and create instance of TrapZone
-	elements = input('Enter number of traps: ')
+	radius = input('Enter radius of traps: ')
 	response = raw_input('Would you like to shrink the zone? [y/n]: ')
 	zone1 = zone
 	if response == "y":
 		erode = True
 		bufAmt = input('How much buffer?: ')
 		zone1 = zone.buffer(-1*abs(bufAmt))
-	tz = trapzone.TrapZone(zone1,obstacles,elements)
+		for obstacle in obstacles:
+			obstacles1 = obstacles1 + [obstacle.buffer(abs(bufAmt))]
+	tz = trapzone.TrapZone(zone1,obstacles1,radius)
 	
 	# Store the corners of the buffered square
 	bounds1 = zone1.bounds
@@ -61,13 +219,17 @@ if __name__ == '__main__':
 	
 	# Prepare the window that will display the map
 	root = tk.Tk()
-	canvas = tk.Canvas(root, width=500, height=500, highlightthickness=0,bg="white")
+	canvas = tk.Canvas(root, width=220, height=220, highlightthickness=0,bg="white")
 	canvas.pack()
 	
 	# Display the points in the window
 	t2.draw_poly(rect1,canvas,'black')
 	t2.draw_poly(zone,canvas,'red')
 	t2.draw_poly(zone1,canvas,'yellow')
+	for obstacle in obstacles1:
+		t2.draw_poly(obstacle,canvas,'black')
+	for obstacle in obstacles:
+		t2.draw_poly(obstacle,canvas,'blue')
 	trapzone.drawPoints(canvas,traps,3,'green')
 	trapzone.drawPoints(canvas,outerTraps,3,'white')
 	
--- trapzone.py
+++ trapzone.py
@@ -17,10 +17,10 @@ class TrapZone(object):
     #         ... the traps get to the no-fly zone.
     # erode - a boolean specifying whether the polygon should shrink a bit
     #         ...to ensure that traps aren't place close to the no-fly zones.
-    def __init__(self, zone, obstacles, num_traps):
-        self.zone = zone
-        self.obstacles = obstacles
-        self.num_traps = num_traps
+    def __init__(self, zone, obstacles, radius):
+		self.zone = zone
+		self.obstacles = obstacles
+		self.radius = radius
         
         
     # function that generates the locations of the traps.
@@ -34,56 +34,45 @@ class TrapZone(object):
     # traps in the polygon than the user specified, then the extras are 
     # randomly removed. 
     def genPoints(self):
-        bounds = self.zone.bounds # get the corners of the rectangle that contain the Polygon
-        width = math.ceil(bounds[2]-bounds[0])
-        height = math.ceil(bounds[3]-bounds[1])
-        fullTraps = False # Distribute the traps in the rectangle 
-        num_points = self.num_traps
-        while not fullTraps:
-            # Generate coordinates in a square
-            points = self.genCoords(num_points,width,height)
-            points = [[x[0]+bounds[0],x[1]+bounds[1]] for x in points]
-            points = self.pointsInZone(points)
-            
-            # Check how many points are located in the zone
-            inZone = len(points[0])
-            if inZone < self.num_traps:
-                # Growth equation for point generation in rectangle (modify)
-                num_points = num_points + self.num_traps
-            else:
-                fullTraps = True  # There are enough traps in the zone
-                random.seed() # Initialize a random seed
-                while inZone != self.num_traps:
-                    index = random.randint(0,inZone-1)
-                    points[0] = points[0][:index] + points[0][index+1 :]
-                    inZone = inZone - 1
-        return points
+		bounds = self.zone.bounds # get the corners of the rectangle that contain the Polygon
+		width = math.ceil(bounds[2]-bounds[0])
+		height = math.ceil(bounds[3]-bounds[1])
+		# generate coordinates in a rectangle
+		points = self.genCoords(width,height)
+		points = [[x[0]+bounds[0],x[1]+bounds[1]] for x in points]
+		return self.pointsInZone(points)
+		
                 
-    def genCoords(self, elements, width, height):
-        # Compute some intermediate numbers
-        py = (height/(height+width))*elements
-        ratio = ((elements*height)/(width*(py**2)))**.5
-        ry = ratio*py # Number of points on y axis (height)
-        inc = height/ry # increment for x and y to make points evenly spaced
-        points = []
-        w = 0
-        while w < (width-inc*.25):
-            h = 0
-            while h < (height-inc*.25):
-                points = points + [(w,h)]
-                h = h + inc
-            w = w + inc
-        # Center the points in the rectangle
-        x_shift = (width-points[len(points)-1][0])/2
-        y_shift = (height-points[len(points)-1][1])/2
-        points = [[x[0]+x_shift,x[1]+y_shift] for x in points]
-        return points
+    def genCoords(self, width, height):
+		
+		# First, use the radius to populate the rectangle
+		inc = 1.8*self.radius
+		points = []
+		w = 0
+		while w < width:
+			h = 0
+			while h < height:
+				points = points + [(w,h)]
+				h = h + inc
+			w = w + inc
+		# center the points in the rectangle
+		x_shift = (width-points[len(points)-1][0])/2
+		y_shift = (height-points[len(points)-1][1])/2
+		points = [[x[0]+x_shift,x[1]+y_shift] for x in points]
+		return points
     
     def pointsInZone(self, points):
         resultList = [[],[]]
         for x in points:
             if self.zone.contains(shapely.geometry.Point(x)):
-                resultList[0] = resultList[0] + [x]
+				if self.obstacles:
+					for obstacle in self.obstacles:
+						if not obstacle.contains(shapely.geometry.Point(x)):
+							resultList[0] = resultList[0] + [x]
+						else: 
+							resultList[1] = resultList[1] + [x]
+				else:
+					resultList[0] = resultList[0] + [x]
             else: 
                 resultList[1] = resultList[1] + [x]
         return resultList
